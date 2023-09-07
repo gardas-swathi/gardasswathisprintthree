99acdae80671ea365610909b4856084a7def7e36
gardas-swathi <135500902+gardas-swathi@users.noreply.github.com>
2023-09-08 01:25:54 +0530

    Update LoginRepo.java

--- watchstore/src/test/main/java/com/example/watchstoreDatabase/Repository/LoginRepo.java
+++ watchstore/src/test/main/java/com/example/watchstoreDatabase/Repository/LoginRepo.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.example.shoeProductDatabase.Entity.Login;
+import com.example.watchstoreDatabase.Entity.Login;
 @Repository
 public interface LoginRepo extends JpaRepository<Login, Integer> {
 
